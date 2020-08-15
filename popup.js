588e9b5966d77c918c092c7b085e4b79f342a66a
Evelyn Yang <evyang@cs.washington.edu>
2020-08-15 14:36:36 -0700

    Moved background into popup.

--- popup.js
+++ popup.js
@@ -1,5 +1,15 @@
+// click event listener for "view all tabs" button on popup
 document.getElementById("view").addEventListener("click", displayTabs);
 
+// display all tabs
 function displayTabs() {
-    // display all tabs
+    chrome.tabs.query({}, function(tabs){
+        let parse = [];
+        tabs.forEach(function(tab) {
+            parse.push(tab);
+            console.log(tab.url + '\n');
+            console.log(tab.windowId);
+        });
+        console.log(parse.length);
+    });
 }
