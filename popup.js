7288697e65da9177330b79a732a300fdb72a73b5
Evelyn Yang <evyang@cs.washington.edu>
2020-08-15 15:32:05 -0700

    Display urls.

--- popup.js
+++ popup.js
@@ -13,7 +13,15 @@ function displayTabs() {
             let faviconUrl = parseUrl[0].concat("//").concat(parseUrl[2]).concat("/favicon.ico");
             favicons.push(faviconUrl);
         });
+
+        for (let i = 0; i < favicons.length; i++) {
+            let btn = document.createElement("BUTTON");
+            btn.innerHTML = favicons[i];
+            document.body.appendChild(btn);
+        }
     });
+
+
 }
 
 // voice assistant
