cac576df2050f72b4aa51202d812ce17ace08e3c
mulepati <sabalmulepati@gmail.com>
2020-08-15 14:01:42 -0700

    something

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.tabs.query({},function(tabs){
     let i = 0;
     tabs.forEach(function(tab){
-      console.log(tab.url);
+      console.log(tab.url + '\n');
+      console.log(tab.windowId);
       i += 1;
     });
     console.log(i);
