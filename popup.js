eeb6e37d07ddcaf83065c44ef5f84d66ce40cabe
Evelyn Yang <evyang@cs.washington.edu>
2020-08-15 20:06:17 -0700

    User can click on icon/title to go to that tab!!!

--- popup.js
+++ popup.js
@@ -2,33 +2,22 @@
 document.getElementById("view").addEventListener("click", displayTabs);
 document.getElementById("voice").addEventListener("click", voiceAssistant);
 
-// display all tabs
+// Display icons and titles of all tabs in all chrome windows
 function displayTabs() {
-    // map:
     // keys: window id
     // values: array of tabs
     let tabInfo = new Map();
 
-    // Get info for all tabs
+    // Get all tabs in each window
     chrome.tabs.query({}, function(tabs) {
-        let allTabs = []; // array of all tabs
-        //let favicons = []; // array of all favicon urls
         tabs.forEach(function(tab) {
-            // add to array of all tabs
-            allTabs.push(tab);
-
-            // parse url and create favicon url
-            // let parseUrl = tab.url.split("/");
-            // let faviconUrl = parseUrl[0].concat("//").concat(parseUrl[2]).concat("/favicon.ico");
-            // favicons.push(faviconUrl);
-
+            // Add window ID to map if it isn't already there
             let windowID = tab.windowId;
             if (!tabInfo.has(windowID)) {
                 tabInfo.set(windowID, [])
             }
 
-            // even indexes = tab urls
-            // odd indexes = tab title
+            // Add tab to array of tabs under window id key
             tabInfo.get(windowID).push(tab);
 
         });
@@ -39,59 +28,59 @@ function displayTabs() {
         let currentWindow = windows.next();
         while (!currentWindow.done) {
             let windowID = currentWindow.value;
-            // WINDOW TITLE
+            // WINDOW TITLE!
             let windowTitle = document.createElement("input");
             windowTitle.placeholder = "Window " + windowCount;
             document.body.appendChild(windowTitle);
 
+            // For each tab under the current window, display favicon and title
             let tabs = tabInfo.get(windowID);
             for (let i = 0; i < tabs.length; i ++) {
                 let tab = tabs[i];
+                let url = tab.url;
 
                 // FAVICON ICONS!
-                let parseUrl = tab.url.split("/");
+                let parseUrl = url.split("/");
                 let faviconUrl = parseUrl[0].concat("//").concat(parseUrl[2]).concat("/favicon.ico");
                 let img = document.createElement("img");
                 img.src = faviconUrl;
                 img.height = 32;
                 img.width = 32;
+                let tabID = "id";
+                img.id = tabID;
+                img.onclick = function(){
+                    chrome.tabs.highlight({windowId: windowID, tabs: i})
+                };
                 img.onerror = function(){
                     img.src = "./32x32.png";
                 };
 
                 // TAB TITLES!
-                let tabTitle = document.createElement("p");
-                tabTitle.innerHTML = tab.title;
+                let title = document.createElement("p");
+                title.id = url;
+                title.innerHTML = tab.title;
+                title.onclick = function(){
+                    chrome.tabs.highlight({windowId: windowID, tabs: i})
+                };
 
                 // Append new elements
                 document.body.appendChild(img);
-                document.body.appendChild(tabTitle);
+                document.body.appendChild(title);
             }
             // Update current window
             currentWindow = windows.next();
             windowCount++;
         }
 
-        // for (let i = 0; i < favicons.length; i++) {
-        //     /*let img = document.createElement("img");
-        //     img.src = favicons[i];
-        //     img.height = 32;
-        //     img.width = 32;
-        //     img.id = i;
-        //     img.onerror = function(){
-        //         img.src = "./32x32.png";
-        //     };*/
-        //
-        //     /*let h = document.createElement("p");
-        //     let t = document.createTextNode(allTabs[i].title);
-        //     h.appendChild(t);
-        //     document.body.appendChild(img);
-        //     document.body.appendChild(h);*/
-        // }
     });
 
 
 }
+
+// go to tab
+// function goToTab(windowID, tabIndex) {
+//     chrome.tabs.highlight({windowId: windowID, tabs: tabIndex})
+// }
 
 // voice assistant
 function voiceAssistant() {
